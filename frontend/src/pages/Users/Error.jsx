50e8430096d66ec8e6a67360f3a020db2a214c08
Laure Graves <graveslaure@gmail.com>
2023-07-20 15:01:07 +0200

    page 404 ok, lien vers retour accueil et style css

--- frontend/src/pages/Users/Error.jsx
+++ frontend/src/pages/Users/Error.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 function Error() {
   return (
-    <div className="text-center text-secondary font-sans text-6xl">
-      Erreur 404 Cette page est introuvable...
+    <div className="error-container">
+      <div className="error-text">Erreur 404 Cette page est introuvable...</div>
       <img
         src="https://media.discordapp.net/attachments/1081687214460780575/1131526714644762654/gateau_404.jpg"
         alt="Error 404"
-        className="mx-auto block"
+        className="error-image"
       />
       <Link to="/" className="custom-button">
         Retour à l'accueil
